ffb0b3511ca8dfd4c9e87968d82f12392575361d
Fonseca-J <148169935+Fonseca-J@users.noreply.github.com>
2024-09-23 21:49:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ * Jogo Jokenpô
+ * @author João Fonseca
+ * @link https://github.com/Fonseca-J/jokenpo.git
+ * @version 1.0
+ */
+
+function jogar() {
+    //validação de campos obrigatórios (radio button)
+    if (document.getElementById('pedra').checked === false && document.getElementById('papel').checked === false && document.getElementById('tesoura').checked === false) {
+        alert("Selecione uma opção")
+        
+    } else {
+        //lógica principal
+        let sorteio = Math.floor(Math.random() * 3) // 0,1,2
+        switch (sorteio) {
+            case 0:
+                document.getElementById('pc').src="img/pcpedra.png"
+                break
+            case 1:
+                document.getElementById('pc').src="img/pcpapel.png"
+                break
+            case 2:
+                document.getElementById('pc').src="img/pctesoura.png"
+                break
+        }
+        // verificar vencedor ou declarar empate
+        if((document.getElementById('pedra').checked === true && sorteio === 0) || (document.getElementById('papel').checked === true && sorteio === 1) || (document.getElementById('tesoura').checked === true && sorteio === 2)){
+            document.getElementById('placar').innerHTML="Empate"
+        } else if ((document.getElementById('pedra').checked === true && sorteio === 2) || (document.getElementById('papel').checked === true && sorteio === 0) || (document.getElementById('tesoura').checked === true && sorteio === 1)) {
+            document.getElementById('placar').innerHTML="Venceu"
+        } else {
+            document.getElementById('placar').innerHTML="Computador venceu"
+        }
+    }
+}
+function limpar(){
+    document.getElementById('pc').src ="img/pc.png"
+    document.getElementById('placar').innerHTML=""
+}
